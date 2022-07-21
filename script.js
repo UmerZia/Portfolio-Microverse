ddab85b691b4167230c54905d8ece13e6620c6a4
UmerZia <omerzia73@gmail.com>
2022-07-22 02:06:27 +0500

    fix-linter-errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const navSlide = () => {
-    const hamburgerMenuOpen = document.getElementsByClassName("navbar__hamburger");
-    const hamburgerMenuExit = document.getElementsByClassName("navbar__opt--cross");
+  const hamburgerMenuOpen = document.getElementsByClassName('navbar__hamburger');
+  const hamburgerMenuExit = document.getElementsByClassName('navbar__opt--cross');
 
-    hamburgerMenuOpen[0].addEventListener("click", ()=>{
-        document.getElementsByClassName("navbar__opt")[0].style.display = "flex";
-    });
+  hamburgerMenuOpen[0].addEventListener('click', () => {
+    document.getElementsByClassName('navbar__opt')[0].style.display = 'flex';
+  });
 
-    hamburgerMenuExit[0].addEventListener("click", ()=> {
-        document.getElementsByClassName("navbar__opt")[0].style.display = "none";
-    });
+  hamburgerMenuExit[0].addEventListener('click', () => {
+    document.getElementsByClassName('navbar__opt')[0].style.display = 'none';
+  });
 };
 
 navSlide();
