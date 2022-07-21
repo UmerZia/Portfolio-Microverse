d1da9d7a3d15ff541d23dfd5c7d796fcbd6ad1c8
UmerZia <omerzia73@gmail.com>
2022-07-22 03:51:58 +0500

    replace-array-with-forEach

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const navSlide = () => {
   const hamburgerMenuOpen = document.getElementsByClassName('navbar__hamburger');
   const hamburgerMenuExit = document.getElementsByClassName('navbar__opt--cross');
+  const hamburgerMenuListItems = document.getElementsByClassName(
+    'navbar__opt__list--item',
+  );
 
   hamburgerMenuOpen[0].addEventListener('click', () => {
     document.getElementsByClassName('navbar__opt')[0].style.display = 'flex';
@@ -8,6 +11,12 @@ const navSlide = () => {
 
   hamburgerMenuExit[0].addEventListener('click', () => {
     document.getElementsByClassName('navbar__opt')[0].style.display = 'none';
+  });
+
+  [...hamburgerMenuListItems].forEach((item) => {
+    item.addEventListener('click', () => {
+      document.getElementsByClassName('navbar__opt')[0].style.display = 'none';
+    });
   });
 };
 
